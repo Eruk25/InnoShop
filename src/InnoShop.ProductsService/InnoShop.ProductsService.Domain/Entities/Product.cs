733eb6b487a84b1f2fe84d19e84251ab04d22b36
Danila <danilamatush72@gmail.com>
2025-11-07 18:39:37 +0300

    feat(domain): add Product model to Domain

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Domain/Entities/Product.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Domain/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace InnoShop.ProductsService.Domain.Entities;
+﻿using InnoShop.ProductsService.Domain.Enums;
+
+namespace InnoShop.ProductsService.Domain.Entities;
 
 public class Product
 {
@@ -6,5 +8,50 @@ public class Product
     public string Title { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public 
+    public Status Status { get; set; }
+    public int UserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public Product(string title, string description, decimal price, int userId)
+    {
+        UpdateTitle(title);
+        UpdateDescription(description);
+        UpdatePrice(price);
+        UpdateStatus(Status.IsActive);
+        UpdateUserId(userId);
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateTitle(string title)
+    {
+        if(string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty", nameof(title));
+        Title = title;
+    }
+
+    public void UpdateDescription(string description)
+    {
+        if(string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Description cannot be empty", nameof(description));
+        Description = description;
+    }
+
+    public void UpdatePrice(decimal price)
+    {
+        if(price <= 0)
+            throw new ArgumentException("Price cannot be negative", nameof(price));
+        Price = price;
+    }
+
+    public void UpdateStatus(Status status)
+    {
+        Status = status;
+    }
+
+    public void UpdateUserId(int userId)
+    {
+        if(userId <= 0)
+            throw new ArgumentException("UserId cannot be negative", nameof(userId));
+        UserId = userId;
+    }
 }
