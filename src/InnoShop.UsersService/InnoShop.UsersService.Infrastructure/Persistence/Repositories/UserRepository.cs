c1995df9b11271f1cdf34bd9b923acf41ed63c03
Danila <danilamatush72@gmail.com>
2025-11-08 13:21:50 +0300

    feat(infrastructure): add UserContext to UserRepository

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,10 +1,18 @@
 ﻿using InnoShop.UsersService.Application.Abstractions.Repositories;
 using InnoShop.UsersService.Domain.Entities;
+using InnoShop.UsersService.Infrastructure.Persistence.DB;
 
 namespace InnoShop.UsersService.Infrastructure.Persistence.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly UserContext _context;
+
+    public UserRepository(UserContext context)
+    {
+        _context = context;
+    }
+    
     public Task<IEnumerable<User>> GetAllAsync()
     {
         throw new NotImplementedException();
