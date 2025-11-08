8993d16322233d641edd6abcc689027f6e089bb0
Danila <danilamatush72@gmail.com>
2025-11-08 14:49:34 +0300

    refactor(application): change returned value from User to User? in IUserRepository

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IUserRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace InnoShop.UsersService.Application.Abstractions.Repositories;
 public interface IUserRepository
 {
     public Task<IEnumerable<User>> GetAllAsync();
-    public Task<User> GetByIdAsync(int id);
+    public Task<User?> GetByIdAsync(int id);
     public Task<int> CreateAsync(User user);
     public Task<User> UpdateAsync(int id, User user);
     public Task<bool> DeleteAsync(int id);
