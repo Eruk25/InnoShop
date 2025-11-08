99320c8cd9497222c06c34e8d056fb12922f200f
Danila <danilamatush72@gmail.com>
2025-11-08 14:27:23 +0300

    feat(application): add IProductRepository interface

---
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using InnoShop.ProductsService.Domain.Entities;
+
+namespace InnoShop.ProductsService.Application.Abstractions.Repositories;
+
+public interface IProductRepository
+{
+    public Task<IEnumerable<Product>> GetAllAsync();
+    public Task<Product> GetByIdAsync(int id);
+    public Task<int> CreateAsync(Product product);
+    public Task<Product> UpdateAsync(int id, Product product);
+    public Task<bool> DeleteAsync(int id);
+}
