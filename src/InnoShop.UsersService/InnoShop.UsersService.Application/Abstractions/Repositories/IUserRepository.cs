f6bbd6d7e300e5e7bf5fc39b964c359115040231
Danila <danilamatush72@gmail.com>
2025-11-08 14:48:34 +0300

    refactor(application): add id argument to Update method in IUserRepository

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IUserRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IUserRepository.cs
@@ -7,6 +7,6 @@ public interface IUserRepository
     public Task<IEnumerable<User>> GetAllAsync();
     public Task<User> GetByIdAsync(int id);
     public Task<int> CreateAsync(User user);
-    public Task<User> UpdateAsync(User user);
+    public Task<User> UpdateAsync(int id, User user);
     public Task<bool> DeleteAsync(int id);
 }
