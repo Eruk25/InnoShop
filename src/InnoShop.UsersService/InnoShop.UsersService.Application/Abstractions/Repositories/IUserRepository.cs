ee855917b3180a5a681845ae9db51c7e7bbab134
Danila <danilamatush72@gmail.com>
2025-11-11 13:08:25 +0300

    feat(application): add ExistByEmailAsync method to Application

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IUserRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IUserRepository.cs
@@ -6,6 +6,7 @@ public interface IUserRepository
 {
     public Task<IEnumerable<User>> GetAllAsync();
     public Task<User?> GetByIdAsync(int id);
+    public Task<bool> ExistByEmailAsync(string email);
     public Task<User> CreateAsync(User user);
     public Task<User> UpdateAsync(int id, User user);
     public Task<bool> DeleteAsync(int id);
