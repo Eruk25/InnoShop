a65685025bf20e2600461ed8f46cd656e07cd208
Danila <danilamatush72@gmail.com>
2025-11-11 12:34:28 +0300

    feat(application): add RegisterUserCommand to Application

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Register/RegisterUserCommand.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Register/RegisterUserCommand.cs
@@ -2,4 +2,5 @@ using MediatR;
 
 namespace InnoShop.UsersService.Application.Users.Register;
 
-public record RegisterUserCommand(string Name, string Email, string Password) : IRequest<bool>;
+public record RegisterUserCommand(string Name, string Email, string Password)
+    : IRequest<RegisterUserResult>;
