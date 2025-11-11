5753ca0b06323da47f30685b04849d251a09b68b
Danila <danilamatush72@gmail.com>
2025-11-11 20:07:45 +0300

    feat(application): add MediatR to UpdateUserByAdminCommand

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserByAdminCommand/UpdateUserByAdminCommand.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserByAdminCommand/UpdateUserByAdminCommand.cs
@@ -1,5 +1,8 @@
+using InnoShop.UsersService.Application.Users.Results;
 using InnoShop.UsersService.Domain.Enums;
+using MediatR;
 
 namespace InnoShop.UsersService.Application.Users.Update.UpdateUserByAdminCommand;
 
-public record UpdateUserByAdminCommand(int Id, Role Role, Status Status);
+public record UpdateUserByAdminCommand(int Id, Role Role, Status Status)
+    : IRequest<UpdateUserProfileResult>;
