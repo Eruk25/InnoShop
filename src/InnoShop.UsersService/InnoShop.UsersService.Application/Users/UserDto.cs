9275dfb1fc490df89a635ce5e8a66de2a08eeca5
Danila <danilamatush72@gmail.com>
2025-11-11 13:56:16 +0300

    refactor(application): add additional fields Status and Role to UserDto in Application

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/UserDto.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/UserDto.cs
@@ -1,3 +1,6 @@
+using InnoShop.UsersService.Domain.Enums;
+
 namespace InnoShop.UsersService.Application.Users;
 
-public record UserDto(int Id, string Name, string Email);
+public record UserDto(int Id, string Name, string Email,
+    Status Status, Role Role);
