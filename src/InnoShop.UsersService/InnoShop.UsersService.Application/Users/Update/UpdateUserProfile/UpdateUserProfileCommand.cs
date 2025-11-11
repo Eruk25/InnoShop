d939f98e661b11ef8d135318f3d8a1ba01970205
Danila <danilamatush72@gmail.com>
2025-11-11 19:48:43 +0300

    feat(application): add UpdateUserProfileCommand to Application

---
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserProfile/UpdateUserProfileCommand.cs
@@ -0,0 +1,5 @@
+using InnoShop.UsersService.Domain.Enums;
+
+namespace InnoShop.UsersService.Application.Users.Update.UpdateUserProfile;
+
+public record UpdateUserProfileCommand(int Id, string Name, string Email, string Password);
