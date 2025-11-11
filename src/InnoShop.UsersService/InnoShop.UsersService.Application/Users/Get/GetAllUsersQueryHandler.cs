193e3503f6b0f8c02d2897b4eede03903fe87837
Danila <danilamatush72@gmail.com>
2025-11-11 13:58:26 +0300

    feat(application): add GetAllUsersQueryHandler to Aplliction

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Get/GetAllUsersQueryHandler.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Get/GetAllUsersQueryHandler.cs
@@ -1,6 +1,23 @@
+using AutoMapper;
+using InnoShop.UsersService.Application.Abstractions.Repositories;
+using MediatR;
+
 namespace InnoShop.UsersService.Application.Users.Get;
 
-public class GetAllUsersQueryHandler
+public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<UserDto>>
 {
-    
+    private readonly IMapper _mapper;
+    private readonly IUserRepository _userRepository;
+
+    public GetAllUsersQueryHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<UserDto>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _userRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<UserDto>>(users);
+    }
 }
