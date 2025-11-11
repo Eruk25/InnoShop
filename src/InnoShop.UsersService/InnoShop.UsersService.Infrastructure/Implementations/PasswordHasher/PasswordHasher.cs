d7a6e89af119f70b20016fc9a2988df2c6a8e67e
Danila <danilamatush72@gmail.com>
2025-11-11 13:07:37 +0300

    feat(infrastructure): implement IPasswordHasher in PasswordHasher

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Implementations/PasswordHasher/PasswordHasher.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Implementations/PasswordHasher/PasswordHasher.cs
@@ -1,6 +1,23 @@
+using InnoShop.UsersService.Application.Abstractions.PasswordHasher;
+
 namespace InnoShop.UsersService.Infrastructure.Implementations.PasswordHasher;
 
-public class PasswordHasher
+public class PasswordHasher : IPasswordHasher
 {
+    private readonly Identity.PasswordHasher.PasswordHasher _hasher;
+
+    public PasswordHasher(Identity.PasswordHasher.PasswordHasher hasher)
+    {
+        _hasher = hasher;
+    }
     
+    public string HashPassword(string password)
+    {
+        return _hasher.HashPassword(password);
+    }
+
+    public bool VerifyHashedPassword(string hashedPassword, string providedPassword)
+    {
+        return _hasher.VerifyHashedPassword(hashedPassword, providedPassword);
+    }
 }
