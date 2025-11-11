1fa980ed2550b0e4a9eaa68c9d0eda25db96a5ac
Danila <danilamatush72@gmail.com>
2025-11-11 20:08:43 +0300

    feat(application): add MediatR to UpdateUserProfileCommand

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserProfile/UpdateUserProfileCommand.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserProfile/UpdateUserProfileCommand.cs
@@ -1,3 +1,7 @@
+using InnoShop.UsersService.Application.Users.Results;
+using MediatR;
+
 namespace InnoShop.UsersService.Application.Users.Update.UpdateUserProfile;
 
-public record UpdateUserProfileCommand(int Id, string Name, string Email, string Password);
+public record UpdateUserProfileCommand(int Id, string Name, string Email, string Password)
+    : IRequest<UpdateUserProfileResult>;
