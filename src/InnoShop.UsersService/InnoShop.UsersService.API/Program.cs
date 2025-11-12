c443e0e11ebe30b98c1af6e88a11c711561dd9bc
Danila <danilamatush72@gmail.com>
2025-11-12 18:19:20 +0300

    feat(presentaion): regitster Presentation, Infrastructure and Application in DI

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Program.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Program.cs
@@ -1,4 +1,13 @@
+using InnoShop.UsersService.API.Extensions;
+using InnoShop.UsersService.Application.Extensions;
+using InnoShop.UsersService.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddPresentation()
+    .AddInfrastructure(builder.Configuration)
+    .AddApplication();
 
 var app = builder.Build();
 
