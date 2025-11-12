60917538569b2d194334ba4c55839e7dbed35249
Danila <danilamatush72@gmail.com>
2025-11-12 12:13:22 +0300

    feat(infrastructure): implement GetByEmailAsync method in UserRepository

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -26,12 +26,20 @@ public class UserRepository : IUserRepository
         return user;
     }
 
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == email);
+        return user;
+    }
+
     public async Task<bool> ExistByEmailAsync(string email)
     {
         var user = await _context.Users
             .FirstOrDefaultAsync(u => u.Email == email);
         return user is not null;
     }
+    
     public async Task<User> CreateAsync(User user)
     {
         await _context.Users.AddAsync(user);
