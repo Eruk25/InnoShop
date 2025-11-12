6b75b81e432ac9ef0409956647998a193f756c9a
Danila <danilamatush72@gmail.com>
2025-11-12 18:16:49 +0300

    refactor(presentation): change extention method name from AddInnoShopUsersServices to AddPresentation

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
@@ -2,7 +2,7 @@ namespace InnoShop.UsersService.API.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInnoShopUsersServices(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
         services.AddOpenApi();
         services.AddControllers();
