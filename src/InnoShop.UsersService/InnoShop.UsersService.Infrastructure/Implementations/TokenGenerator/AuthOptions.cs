53c3a99f2c9a14507048550ad031adbbed995654
Danila <danilamatush72@gmail.com>
2025-11-12 12:44:14 +0300

    feat(infrastructure): change ExpirationTime from string to TimeSpan in AuthOptions

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Implementations/TokenGenerator/AuthOptions.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Implementations/TokenGenerator/AuthOptions.cs
@@ -5,5 +5,5 @@ public class AuthOptions
     public string Issuer { get; init; } = string.Empty;
     public string Audience { get; init; } = string.Empty;
     public string SecretKey { get; init; } = string.Empty;
-    public string ExpirationTime { get; init; } = string.Empty;
+    public TimeSpan ExpirationTime { get; init; }
 }
