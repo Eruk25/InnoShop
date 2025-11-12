25ba0084f26ae164d508dc5fcb21c9395b8f2037
Danila <danilamatush72@gmail.com>
2025-11-12 17:57:48 +0300

    refactor(presentation): change namespaces

--- src/InnoShop.UsersService/InnoShop.UsersService.API/DTOs/Requests/Register/RegisterUserRequest.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/DTOs/Requests/Register/RegisterUserRequest.cs
@@ -1,3 +1,3 @@
-namespace InnoShop.UsersService.API.DTOs.Requests;
+namespace InnoShop.UsersService.API.DTOs.Requests.Register;
 
 public record RegisterUserRequest(string Name, string Email, string Password);
--- src/InnoShop.UsersService/InnoShop.UsersService.API/DTOs/Responses/Register/RegisterUserResponse.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/DTOs/Responses/Register/RegisterUserResponse.cs
@@ -1,3 +1,3 @@
-namespace InnoShop.UsersService.API.DTOs.Responses;
+namespace InnoShop.UsersService.API.DTOs.Responses.Register;
 
 public record RegisterUserResponse(int Id, string Name, string Email);
