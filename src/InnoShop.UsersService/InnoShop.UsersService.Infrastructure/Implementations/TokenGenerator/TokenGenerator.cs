dd66e7b4899dd541d44cd1d77f8100421ea0a55f
Danila <danilamatush72@gmail.com>
2025-11-12 12:45:06 +0300

    feat(infrastructure): add TokenGenerator to infrastructure

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Implementations/TokenGenerator/TokenGenerator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Implementations/TokenGenerator/TokenGenerator.cs
@@ -1,6 +1,41 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using InnoShop.UsersService.Application.Abstractions.TokenGenerator;
+using InnoShop.UsersService.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
 namespace InnoShop.UsersService.Infrastructure.Implementations.TokenGenerator;
 
-public class TokenGenerator
+public class TokenGenerator : ITokenGenerator
 {
-    
+    private readonly IOptions<AuthOptions> _options;
+
+    public TokenGenerator(IOptions<AuthOptions> options)
+    {
+        _options = options;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        };
+        
+        var jwt = new JwtSecurityToken(
+            issuer: _options.Value.Issuer,
+            audience: _options.Value.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(_options.Value.ExpirationTime),
+            signingCredentials:
+            new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_options.Value.SecretKey)),
+                SecurityAlgorithms.HmacSha256));
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
 }
