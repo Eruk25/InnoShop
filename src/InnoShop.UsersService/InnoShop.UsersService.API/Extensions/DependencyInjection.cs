9d193dd1294f0d99691c6e055e9a7caa19d72c99
Danila <danilamatush72@gmail.com>
2025-11-13 22:37:02 +0300

    refactor(presentation): remove service.AddOpenApi from DI

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
@@ -4,7 +4,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddOpenApi();
         services.AddControllers();
         services.AddSwaggerGen();
 
