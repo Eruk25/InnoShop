5e487195b5477ac05c7c5736fde6d4a738e3f98b
Danila <danilamatush72@gmail.com>
2025-11-15 22:56:31 +0300

    feat(presentation): register Authentication and Authorization in DI

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
@@ -1,10 +1,15 @@
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace InnoShop.UsersService.API.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddControllers();
         services.AddSwaggerGen(opt =>
@@ -33,6 +38,24 @@ public static class DependencyInjection
                 }
             });
         });
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = configuration["AuthOptions:Issuer"],
+                    ValidAudience = configuration["AuthOptions:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["AuthOptions:SecretKey"]!)),
+                    RoleClaimType = ClaimTypes.Role
+                };
+            });
+        services.AddAuthorization();
 
         return services;
     }
