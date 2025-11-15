10900b349aaa2157e4c26d76ca9a44d7c8c59838
Danila <danilamatush72@gmail.com>
2025-11-15 22:53:56 +0300

    feat(presentation): add Authorize to Swagger

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace InnoShop.UsersService.API.Extensions;
 
 public static class DependencyInjection
@@ -5,7 +7,32 @@ public static class DependencyInjection
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
         services.AddControllers();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(opt =>
+        {
+            opt.SwaggerDoc("v1", new OpenApiInfo() { Title = "InnoShop", Version = "v1" });
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new List<string>()
+                }
+            });
+        });
 
         return services;
     }
