5a464318a925bb2558e86daf8f4eddfd1bffd192
Danila <danilamatush72@gmail.com>
2025-11-15 23:11:58 +0300

    feat(presentation): add UpdateUserProfileAsync to UserController

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Controllers/UsersController.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Controllers/UsersController.cs
@@ -1,6 +1,32 @@
+using System.Security.Claims;
+using InnoShop.UsersService.API.DTOs.Requests.UpdateProfile;
+using InnoShop.UsersService.API.Extensions;
+using InnoShop.UsersService.Application.Users.Update.UpdateUserProfile;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InnoShop.UsersService.API.Controllers;
 
-public class UsersController
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
-    
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPatch]
+    [Authorize]
+    [Route("me")]
+    public async Task<IActionResult> UpdateUserProfileAsync([FromBody] UpdateUserProfileRequest request)
+    {
+        var userId = User.GetUserId();
+        await _mediator.Send(new UpdateUserProfileCommand(userId, request.Name,
+            request.Email, request.Password));
+        return NoContent();
+    }
 }
