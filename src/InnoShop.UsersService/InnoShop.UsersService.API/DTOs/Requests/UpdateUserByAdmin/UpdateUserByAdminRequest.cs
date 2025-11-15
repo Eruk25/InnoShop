15fec34b61f4e582558a563e2bb890414a2befe4
Danila <danilamatush72@gmail.com>
2025-11-15 23:22:29 +0300

    feat(presentation): add UpdateUserByAdimRequest dto to API

---
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/DTOs/Requests/UpdateUserByAdmin/UpdateUserByAdminRequest.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+using InnoShop.UsersService.Domain.Enums;
+
+namespace InnoShop.UsersService.API.DTOs.Requests.UpdateUserByAdmin;
+
+public class UpdateUserByAdminRequest
+{
+    public int Id { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Role Role { get; set; }
+}
