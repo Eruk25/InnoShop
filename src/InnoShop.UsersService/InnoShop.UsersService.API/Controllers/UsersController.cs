fcaf947ed639ed2c38c37ea181b58c6e6e30658e
Danila <danilamatush72@gmail.com>
2025-11-15 23:41:10 +0300

    feat(presentation): add UpdateAsync method to UserController

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Controllers/UsersController.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using System.Security.Claims;
 using InnoShop.UsersService.API.DTOs.Requests.UpdateProfile;
+using InnoShop.UsersService.API.DTOs.Requests.UpdateUserByAdmin;
 using InnoShop.UsersService.API.Extensions;
+using InnoShop.UsersService.Application.Users.Update.UpdateUserByAdmin;
 using InnoShop.UsersService.Application.Users.Update.UpdateUserProfile;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -27,6 +29,16 @@ public class UsersController : ControllerBase
         var userId = User.GetUserId();
         await _mediator.Send(new UpdateUserProfileCommand(userId, request.Name,
             request.Email, request.Password));
+        return NoContent();
+    }
+
+    [HttpPatch]
+    [Authorize(Roles = "Admin")]
+    [Route("id")]
+    public async Task<IActionResult> UpdateAsync([FromBody] UpdateUserByAdminRequest request)
+    {
+        var result = await _mediator.Send(new UpdateUserByAdminCommand(
+            request.Id, request.Role, request.Status));
         return NoContent();
     }
 }
