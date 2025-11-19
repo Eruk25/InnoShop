88457ccb3df799ab91e6bdfdb0ea546854f06075
Danila <danilamatush72@gmail.com>
2025-11-19 19:53:51 +0300

    feat(infrastructure): register ProductContext in DI

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Infrastructure/Extensions/DependencyInjection.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Infrastructure/Extensions/DependencyInjection.cs
@@ -1,6 +1,18 @@
+using InnoShop.ProductsService.Infrastructure.Persistence.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace InnoShop.ProductsService.Infrastructure.Extensions;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ProductContext>(opt =>
+            opt.UseSqlServer(configuration.GetConnectionString("SqlConnection")));
+        
+        return services;
+    }
 }
