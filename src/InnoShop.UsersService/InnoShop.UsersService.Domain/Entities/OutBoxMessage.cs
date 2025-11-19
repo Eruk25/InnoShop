5907714c7d22d23123557ba2cf736030a0d6073a
Danila <danilamatush72@gmail.com>
2025-11-19 10:32:20 +0300

    refactor(domain): remove with ctor Id in OutBoxMessage

--- src/InnoShop.UsersService/InnoShop.UsersService.Domain/Entities/OutBoxMessage.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Domain/Entities/OutBoxMessage.cs
@@ -1,8 +1,8 @@
 namespace InnoShop.UsersService.Domain.Entities;
 
-public class OutBoxMessage(Guid id, string type, string payload)
+public class OutBoxMessage(string type, string payload)
 {
-    public Guid Id { get; private set; } = id;
+    public Guid Id { get; private set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     public string Type { get; private set; } = type;
     public string Payload { get; private set; } = payload;
