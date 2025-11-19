63a27a4ba990e08c50c974f891fa23b7c2c3f0b3
Danila <danilamatush72@gmail.com>
2025-11-19 18:49:42 +0300

    refactor(application): change returned value and remove excess argument in Update method

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
@@ -7,6 +7,6 @@ public interface IProductRepository
     public Task<IEnumerable<Product>> GetAllAsync();
     public Task<Product?> GetByIdAsync(int id);
     public Task<int> CreateAsync(Product product);
-    public Task<Product> UpdateAsync(int id, Product product);
+    public Task UpdateAsync(Product product);
     public Task<bool> DeleteAsync(int id);
 }
