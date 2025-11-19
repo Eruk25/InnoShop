96ea23359683e6c14fb0f9aaf65b43e5b25d443b
Danila <danilamatush72@gmail.com>
2025-11-19 12:23:32 +0300

    feat(infrastructure): add UpdateOutBoxMessage migration

---
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Migrations/20251119092243_UpdateOutBoxMessage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoShop.UsersService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOutBoxMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProcessedAt",
+                table: "OutBoxMessages",
+                newName: "ProcessedOn");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "OutBoxMessages",
+                newName: "OccurredOn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProcessedOn",
+                table: "OutBoxMessages",
+                newName: "ProcessedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "OccurredOn",
+                table: "OutBoxMessages",
+                newName: "CreatedAt");
+        }
+    }
+}
