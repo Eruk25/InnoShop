8665bb75478a45bf7af158f434e75fc5a498e98d
Danila <danilamatush72@gmail.com>
2025-11-19 11:30:26 +0300

    feat(domain): change fields from private set to set

--- src/InnoShop.UsersService/InnoShop.UsersService.Domain/Entities/OutBoxMessage.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Domain/Entities/OutBoxMessage.cs
@@ -2,10 +2,10 @@ namespace InnoShop.UsersService.Domain.Entities;
 
 public class OutBoxMessage(string type, string payload)
 {
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
-    public string Type { get; private set; } = type;
-    public string Payload { get; private set; } = payload;
-    public DateTime? ProcessedAt { get; private set; }
-    public string? Error { get; private set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime OccurredOn { get; set; } = DateTime.UtcNow;
+    public string Type { get; set; } = type;
+    public string Payload { get; set; } = payload;
+    public DateTime? ProcessedOn { get; set; }
+    public string? Error { get; set; }
 }
