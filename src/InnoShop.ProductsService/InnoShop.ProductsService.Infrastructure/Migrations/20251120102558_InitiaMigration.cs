f1a2d8e2c84ce0035e7e2be8d9dc558493f7968f
Danila <danilamatush72@gmail.com>
2025-11-20 13:37:22 +0300

    feat(infrastructure): add InitialMigration for ProductService

---
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Infrastructure/Migrations/20251120102558_InitiaMigration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoShop.ProductsService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitiaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_Products_Title",
+                table: "Products",
+                column: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
