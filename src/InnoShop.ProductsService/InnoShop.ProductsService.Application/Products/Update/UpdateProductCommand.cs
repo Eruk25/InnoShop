6ea225544a2c416c6a33f35b451c64e2fb1660fb
Danila <danilamatush72@gmail.com>
2025-11-20 16:17:10 +0300

    refactor(application): change description type from string to string? in UpdateProductCommand

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Update/UpdateProductCommand.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Update/UpdateProductCommand.cs
@@ -2,5 +2,5 @@ using MediatR;
 
 namespace InnoShop.ProductsService.Application.Products.Update;
 
-public record UpdateProductCommand(int Id, string? Title, string Description,
+public record UpdateProductCommand(int Id, string? Title, string? Description,
     decimal? Price) : IRequest;
