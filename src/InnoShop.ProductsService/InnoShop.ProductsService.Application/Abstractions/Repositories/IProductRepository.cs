e0e7e10543319f3522187d06b09dfc459c1b214d
Danila <danilamatush72@gmail.com>
2025-11-20 14:57:26 +0300

    refactor(application): change returned value from Product to void in Create method

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
@@ -6,7 +6,7 @@ public interface IProductRepository
 {
     public Task<IEnumerable<Product>> GetAllAsync();
     public Task<Product?> GetByIdAsync(int id);
-    public Task<Product> CreateAsync(Product product);
+    public Task CreateAsync(Product product);
     public Task UpdateAsync(Product product);
     public Task<bool> DeleteAsync(int id);
 }
