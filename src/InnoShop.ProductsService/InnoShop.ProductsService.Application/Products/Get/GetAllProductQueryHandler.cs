e40a907d2d670cbcf96cda957960bdee412367ec
Danila <danilamatush72@gmail.com>
2025-11-20 14:01:04 +0300

    feat(application): add IMapper to GetAllPriductsQueryHandler

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Get/GetAllProductQueryHandler.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Get/GetAllProductQueryHandler.cs
@@ -1,6 +1,23 @@
+using AutoMapper;
+using InnoShop.ProductsService.Application.Abstractions.Repositories;
+using MediatR;
+
 namespace InnoShop.ProductsService.Application.Products.Get;
 
-public class GetAllProductQueryHandler
+public class GetAllProductQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<ProductDto>>
 {
-    
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+    public GetAllProductQueryHandler(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ProductDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await _productRepository.GetAllAsync();
+        
+        
+    }
 }
