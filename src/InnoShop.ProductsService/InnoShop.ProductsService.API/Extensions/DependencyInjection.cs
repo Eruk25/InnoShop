df57744bc567d8bfb136c3bf48b9d6bc7829803c
Danila <danilamatush72@gmail.com>
2025-11-20 17:33:10 +0300

    feat(presentation): register JwtAuth in DI

--- src/InnoShop.ProductsService/InnoShop.ProductsService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.API/Extensions/DependencyInjection.cs
@@ -1,12 +1,31 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 namespace InnoShop.ProductsService.API.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddControllers();
         services.AddOpenApi();
-        
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["AuthOptions:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = configuration["AuthOptions:Audience"],
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["AuthOptions:SecretKey"])),
+                };
+            });
         return services;
     }
 }
