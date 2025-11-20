8d00043244de0a18479a3cc0cc11bc941f537e62
Danila <danilamatush72@gmail.com>
2025-11-20 16:29:38 +0300

    feat(presentation): register Presentation in DI

--- src/InnoShop.ProductsService/InnoShop.ProductsService.API/Program.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.API/Program.cs
@@ -1,8 +1,11 @@
+using InnoShop.ProductsService.API.Extensions;
 using InnoShop.ProductsService.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services
+    .AddPresentation()
+    .AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
