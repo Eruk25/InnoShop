fe49b9d1f687df94192d6feb2232ae0a8df1efcd
Danila <danilamatush72@gmail.com>
2025-11-20 15:02:27 +0300

    feat(application): add DeleteProductCommandHandler to Application

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Delete/DeleteProductCommandHandler.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Delete/DeleteProductCommandHandler.cs
@@ -1,6 +1,24 @@
+using InnoShop.ProductsService.Application.Abstractions.Repositories;
+using MediatR;
+
 namespace InnoShop.ProductsService.Application.Products.Delete;
 
-public class DeleteProductCommandHandler
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
 {
+    private readonly IProductRepository _productRepository;
     
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.Id);
+        
+        if(product is null)
+            throw new KeyNotFoundException($"Product with id {request.Id} not found");
+        
+        await _productRepository.DeleteAsync(product);
+    }
 }
