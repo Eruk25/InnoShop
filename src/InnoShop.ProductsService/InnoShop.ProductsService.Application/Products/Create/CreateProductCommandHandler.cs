7017361cab07edca184bcd91cfa319559d9f5721
Danila <danilamatush72@gmail.com>
2025-11-20 14:48:12 +0300

    feat(application): add CreateProductCommandHandler

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Create/CreateProductCommandHandler.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Create/CreateProductCommandHandler.cs
@@ -1,6 +1,33 @@
+using AutoMapper;
+using InnoShop.ProductsService.Application.Abstractions.Repositories;
+using InnoShop.ProductsService.Domain.Entities;
+using MediatR;
+
 namespace InnoShop.ProductsService.Application.Products.Create;
 
-public class CreateProdcutCommandHandler :
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductDto>
 {
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
     
+    public CreateProductCommandHandler(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ProductDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        if(request is null)
+            throw new ArgumentException(nameof(request));
+
+        var product = new Product(
+            request.Title,
+            request.Description,
+            request.Price,
+            request.UserId);
+        
+        var createdProduct = await _productRepository.CreateAsync(product);
+        return _mapper.Map<ProductDto>(createdProduct);
+    }
 }
