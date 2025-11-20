a2a5a74c5972cba41cdc95b5a4632f0e8bc3baf5
Danila <danilamatush72@gmail.com>
2025-11-20 16:34:44 +0300

    feat(presentation): register Application in DI

--- src/InnoShop.ProductsService/InnoShop.ProductsService.API/Program.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.API/Program.cs
@@ -1,10 +1,12 @@
 using InnoShop.ProductsService.API.Extensions;
+using InnoShop.ProductsService.Application.Extensions;
 using InnoShop.ProductsService.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddPresentation()
+    .AddApplication()
     .AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
