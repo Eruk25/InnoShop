3296fbcb3580d18727725fbf3ffd66d43e093e4e
Danila <danilamatush72@gmail.com>
2025-11-20 15:19:20 +0300

    feat(application): add UpdateCommandHandler to Application

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Update/UpdateProductCommandHandler.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Update/UpdateProductCommandHandler.cs
@@ -1,7 +1,25 @@
+using InnoShop.ProductsService.Application.Abstractions.Repositories;
+using MediatR;
+
 namespace InnoShop.ProductsService.Application.Products.Update;
 
-public class UpdateProductCommandHandler
+public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
 {
-    
-    
+    private readonly IProductRepository _productRepository;
+
+    public UpdateProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.Id);
+        
+        if(product is null)
+            throw new KeyNotFoundException($"Product with id {request.Id} not found");
+        
+        product.Update(request.Title, request.Description, request.Price);
+        await _productRepository.UpdateAsync(product);
+    }
 }
