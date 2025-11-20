e572fd9c81805e3650388f2052b7eb91363402b6
Danila <danilamatush72@gmail.com>
2025-11-20 14:33:26 +0300

    feat(application): add CreateProductCommand to Application

---
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Create/CreateProductCommand.cs
@@ -0,0 +1,6 @@
+using MediatR;
+
+namespace InnoShop.ProductsService.Application.Products.Create;
+
+public record CreateProductCommand(string Title, string Description,
+    decimal Price, int UserI) : IRequest<ProductDto>;
