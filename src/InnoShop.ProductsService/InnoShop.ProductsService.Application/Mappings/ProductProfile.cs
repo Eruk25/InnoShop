fc82d5f48021bf2918acf9e6f601d0b99505b2a1
Danila <danilamatush72@gmail.com>
2025-11-20 13:59:10 +0300

    feat(application): add ProductProfile to Application

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Mappings/ProductProfile.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Mappings/ProductProfile.cs
@@ -1,6 +1,14 @@
+using AutoMapper;
+using InnoShop.ProductsService.Application.Products;
+using InnoShop.ProductsService.Domain.Entities;
+
 namespace InnoShop.ProductsService.Application.Mappings;
 
-public class ProductProfile
+public class ProductProfile : Profile
 {
-    
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>()
+            .ReverseMap();
+    }
 }
