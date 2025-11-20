a43e90492d87d7648b93503b4c84848ed096a3ab
Danila <danilamatush72@gmail.com>
2025-11-20 15:05:22 +0300

    refacto(application): remove IMapper and change returned value from ProductDto to void in CreateProductCommandHandler

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Create/CreateProductCommandHandler.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Create/CreateProductCommandHandler.cs
@@ -5,18 +5,16 @@ using MediatR;
 
 namespace InnoShop.ProductsService.Application.Products.Create;
 
-public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductDto>
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand>
 {
     private readonly IProductRepository _productRepository;
-    private readonly IMapper _mapper;
     
-    public CreateProductCommandHandler(IProductRepository productRepository, IMapper mapper)
+    public CreateProductCommandHandler(IProductRepository productRepository)
     {
         _productRepository = productRepository;
-        _mapper = mapper;
     }
 
-    public async Task<ProductDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
         if(request is null)
             throw new ArgumentException(nameof(request));
@@ -27,7 +25,6 @@ public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand,
             request.Price,
             request.UserId);
         
-        var createdProduct = await _productRepository.CreateAsync(product);
-        return _mapper.Map<ProductDto>(createdProduct);
+        await _productRepository.CreateAsync(product);
     }
 }
