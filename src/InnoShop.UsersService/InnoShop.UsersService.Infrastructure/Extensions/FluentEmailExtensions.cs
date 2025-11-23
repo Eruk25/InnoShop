2ccc1774ac508ae90946b167a164b7d42c3c276d
Danila <danilamatush72@gmail.com>
2025-11-23 19:46:12 +0300

    feat(infrastructure): add FluentEmailExtensions

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Extensions/FluentEmailExtensions.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Extensions/FluentEmailExtensions.cs
@@ -1,6 +1,21 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace InnoShop.UsersService.Infrastructure.Extensions;
 
-public class FluentEmailExtensions
+public static class FluentEmailExtensions
 {
-    
+    public static void AddFluentEmail(this IServiceCollection services, IConfiguration configuration)
+    {
+        var emailSettings = configuration.GetSection("Email");
+
+        var defaultEmail = emailSettings["DefaultFromEmail"];
+        var host = emailSettings["Host"];
+        var port = emailSettings.GetValue<int>("Port");
+        var username = emailSettings["Username"];
+        var password = emailSettings["Password"];
+
+        services.AddFluentEmail(defaultEmail)
+            .AddSmtpSender(host, port, username, password);
+    }
 }
