45c2454035374069da2afe4ad46869b33d667c70
Danila <danilamatush72@gmail.com>
2025-11-23 19:54:23 +0300

    refactor(application): remove message argument with SendEmailAsync

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/EmailSender/IEmailSender.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/EmailSender/IEmailSender.cs
@@ -2,5 +2,5 @@ namespace InnoShop.UsersService.Application.Abstractions.EmailSender;
 
 public interface IEmailSender
 {
-    public Task SendEmailAsync(string email, string subject, string message);
+    public Task SendEmailAsync(string email, string subject);
 }
