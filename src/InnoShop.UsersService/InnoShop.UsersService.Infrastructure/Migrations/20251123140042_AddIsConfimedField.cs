2cc39e2955b07406d819c3e69a0638ad83c81000
Danila <danilamatush72@gmail.com>
2025-11-23 17:01:49 +0300

    feat(infrastrucure): add AddIsConfirmedField migration

---
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Migrations/20251123140042_AddIsConfimedField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoShop.UsersService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsConfimedField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirmed",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "Users");
+        }
+    }
+}
