1f7b25a567a2856b6537f5eb09b82a324a5ead3a
Danila <danilamatush72@gmail.com>
2025-11-23 21:02:52 +0300

    feat(infrastructure): implement IEmailVerificationTokenRepository in EmailVerificationTokenRepository

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/EmailVerificationTokenRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/EmailVerificationTokenRepository.cs
@@ -1,6 +1,21 @@
+using InnoShop.UsersService.Application.Abstractions.Repositories;
+using InnoShop.UsersService.Domain.Entities;
+using InnoShop.UsersService.Infrastructure.Persistence.DB;
+
 namespace InnoShop.UsersService.Infrastructure.Persistence.Repositories;
 
-public class EmailVeificationTokenRepository : 
+public class EmailVerificationTokenRepository : IEmailVerificationTokenRepository 
 {
+    private readonly UserContext _context;
+
+    public EmailVerificationTokenRepository(UserContext context)
+    {
+        _context = context;
+    }
     
+    public async Task CreateAsync(EmailVerificationToken token)
+    {
+        await _context.EmailVerificationTokens.AddAsync(token);
+        await _context.SaveChangesAsync();
+    }
 }
