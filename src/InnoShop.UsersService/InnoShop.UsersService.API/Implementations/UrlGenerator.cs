0fcbc15035e2533ef3f760e653a32cd80adebee1
Danila <danilamatush72@gmail.com>
2025-11-23 20:47:32 +0300

    feat(presentation): implement IUrlGenerator in UrlGenerator

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Implementations/UrlGenerator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Implementations/UrlGenerator.cs
@@ -1,6 +1,28 @@
+using InnoShop.UsersService.Application.Abstractions.UrlGenerator;
+using InnoShop.UsersService.Domain.Entities;
+
 namespace InnoShop.UsersService.API.Implementations;
 
-public class UrlGenerator
+public class UrlGenerator : IUrlGenerator
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly LinkGenerator _linkGenerator;
+
+    public UrlGenerator(IHttpContextAccessor httpContextAccessor, LinkGenerator linkGenerator)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _linkGenerator = linkGenerator;
+    }
     
+    public string GenerateLink(EmailVerificationToken emailVerificationToken)
+    {
+        string? verificationLink = _linkGenerator.GetUriByAction(
+            action: "VerifyEmail",
+            controller: "Auth",
+            values: new { token = emailVerificationToken.Id},
+            scheme: _httpContextAccessor.HttpContext!.Request.Scheme,
+            host: _httpContextAccessor.HttpContext.Request.Host);
+        
+        return verificationLink ?? throw new Exception("Could not generate link");
+    }
 }
