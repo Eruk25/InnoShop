157a03792e5d26b2985f1049aee3f6b50f198ac0
Danila <danilamatush72@gmail.com>
2025-11-23 20:53:17 +0300

    feat(application): add IEmailSender and IUrlGenerator to RegisterUserCommandHandler

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Register/RegisterUserCommandHandler.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Register/RegisterUserCommandHandler.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using InnoShop.UsersService.Application.Abstractions.EmailSender;
 using InnoShop.UsersService.Application.Abstractions.PasswordHasher;
 using InnoShop.UsersService.Application.Abstractions.Repositories;
+using InnoShop.UsersService.Application.Abstractions.UrlGenerator;
 using InnoShop.UsersService.Application.Users.Results;
 using InnoShop.UsersService.Domain.Entities;
 using InnoShop.UsersService.Domain.Exceptions;
@@ -13,13 +15,18 @@ public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, R
     private readonly IMapper _mapper;
     private readonly IUserRepository _userRepository;
     private readonly IPasswordHasher _passwordHasher;
+    private readonly IEmailSender _emailSender;
+    private readonly IUrlGenerator _urlGenerator;
 
     public RegisterUserCommandHandler(IUserRepository userRepository,
-        IPasswordHasher passwordHasher, IMapper mapper)
+        IPasswordHasher passwordHasher, IMapper mapper,
+        IEmailSender emailSender, IUrlGenerator urlGenerator)
     {
         _userRepository = userRepository;
         _passwordHasher = passwordHasher;
         _mapper = mapper;
+        _emailSender = emailSender;
+        _urlGenerator = urlGenerator;
     }
 
     public async Task<RegisterUserResult> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -32,6 +39,16 @@ public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, R
         
         var user = new User(request.Name, request.Email, _passwordHasher.HashPassword(request.Password));
         var createdUser = await _userRepository.CreateAsync(user);
+
+        var token = new EmailVerificationToken
+        {
+            Id = Guid.NewGuid(),
+            UserId = user.Id,
+            CreatedAt = DateTime.UtcNow,
+            ExpiresAt = DateTime.UtcNow.AddMinutes(30)
+        };
+        string verificationLink = _urlGenerator.GenerateLink(token);
+        await _emailSender.SendEmailAsync(user.Email, verificationLink);
         
         return _mapper.Map<RegisterUserResult>(createdUser);
     }
