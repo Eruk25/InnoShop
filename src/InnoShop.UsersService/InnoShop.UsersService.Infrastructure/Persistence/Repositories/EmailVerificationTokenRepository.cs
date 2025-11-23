1bf56ceaa66b20a28f55bcc1b211361327501007
Danila <danilamatush72@gmail.com>
2025-11-23 22:28:41 +0300

    feat(application): implement GetById and Delete method

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/EmailVerificationTokenRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/EmailVerificationTokenRepository.cs
@@ -1,6 +1,7 @@
 using InnoShop.UsersService.Application.Abstractions.Repositories;
 using InnoShop.UsersService.Domain.Entities;
 using InnoShop.UsersService.Infrastructure.Persistence.DB;
+using Microsoft.EntityFrameworkCore;
 
 namespace InnoShop.UsersService.Infrastructure.Persistence.Repositories;
 
@@ -12,10 +13,24 @@ public class EmailVerificationTokenRepository : IEmailVerificationTokenRepositor
     {
         _context = context;
     }
-    
+
+    public Task<EmailVerificationToken?> GetByIdAsync(Guid tokenId)
+    {
+        var token = _context.EmailVerificationTokens
+            .Include(e => e.User)
+            .FirstOrDefaultAsync(e => e.Id == tokenId);
+        return token;
+    }
+
     public async Task CreateAsync(EmailVerificationToken token)
     {
         await _context.EmailVerificationTokens.AddAsync(token);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(EmailVerificationToken token)
+    {
+        _context.EmailVerificationTokens.Remove(token);
         await _context.SaveChangesAsync();
     }
 }
