545f47fe74df95aa4253fcc6cccd0dbc1bb90757
Danila <danilamatush72@gmail.com>
2025-11-24 17:49:17 +0300

    feat(application): add GetByUserId method and change argument in delete method from Product to IEnumereble<Product>

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
@@ -6,7 +6,8 @@ public interface IProductRepository
 {
     public Task<IEnumerable<Product>> GetAllAsync();
     public Task<Product?> GetByIdAsync(int id);
+    public Task<IEnumerable<Product>> GetByUserIdAsync(int userId);
     public Task CreateAsync(Product product);
     public Task UpdateAsync(Product product);
-    public Task DeleteAsync(Product product);
+    public Task DeleteAsync(IEnumerable<Product> products);
 }
