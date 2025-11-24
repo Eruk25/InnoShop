0aa8812703e457523c713eaf9390416937196da5
Danila <danilamatush72@gmail.com>
2025-11-24 12:43:58 +0300

    feat(presentation): register HettpContextAccessor in DI

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
@@ -59,6 +59,7 @@ public static class DependencyInjection
             });
         services.AddAuthorization();
         services.AddScoped<IUrlGenerator, UrlGenerator>();
+        services.AddHttpContextAccessor();
         return services;
     }
 }
