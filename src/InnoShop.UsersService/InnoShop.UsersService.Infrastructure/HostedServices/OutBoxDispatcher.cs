53dc5660fcc7883743b4089c7d4640c839e37ece
Danila <danilamatush72@gmail.com>
2025-11-27 19:52:01 +0300

    refactor(infrastructure): add ITokenService and change method from Delete to Post

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/HostedServices/OutBoxDispatcher.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/HostedServices/OutBoxDispatcher.cs
@@ -1,5 +1,7 @@
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using InnoShop.UsersService.Application.Abstractions.TokenGenerator;
 using InnoShop.UsersService.Domain.Primitives;
 using InnoShop.UsersService.Infrastructure.Persistence.DB;
 using Microsoft.EntityFrameworkCore;
@@ -11,20 +13,21 @@ namespace InnoShop.UsersService.Infrastructure.HostedServices;
 public class OutBoxDispatcher : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
-    private readonly HttpClient _httpClient = new();
+    private readonly HttpClient _httpClient;
 
     public OutBoxDispatcher(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
+        _httpClient = new HttpClient();
     }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var scope = _serviceProvider.CreateScope();
+            using var scope = _serviceProvider.CreateScope();
             var db = scope.ServiceProvider.GetRequiredService<UserContext>();
-
+            var tokenService = scope.ServiceProvider.GetRequiredService<ITokenService>();
             var messages = await db.OutBoxMessages
                 .Where(m => m.ProcessedOn == null)
                 .OrderBy(m => m.OccurredOn)
@@ -35,11 +38,16 @@ public class OutBoxDispatcher : BackgroundService
             {
                 try
                 {
-                    var payload = JsonSerializer.Deserialize<UserDeletedEvent>(msg.Payload);
-                    var response = await _httpClient.DeleteAsync(
-                        $"http://localhost:5188/api/products/{payload.UserId}/delete",
+                    var token = await tokenService.GenerateServiceTokenAsync(
+                        "mysecretKeysadjaksjdkjdaskdjakldjkasdkajkldsjksakdlakdslalddsadadsdas", "UserService");
+                     _httpClient.DefaultRequestHeaders.Authorization = 
+                         new AuthenticationHeaderValue("Bearer", token);
+                    var payload = JsonSerializer.Deserialize<UserDeletedEvent>(msg.Payload)
+                        ?? throw new Exception("Payload is null");
+                    var response = await _httpClient.PostAsJsonAsync(
+                        $"http://localhost:5188/api/products/{payload.UserId}/soft-delete",
+                        payload,
                         stoppingToken);
-                    
                     response.EnsureSuccessStatusCode();
                     msg.ProcessedOn = DateTime.UtcNow;
                 }
