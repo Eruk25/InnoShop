773d28935ef084ea0aec6fe6f74812a35b2a8cf7
Danila <danilamatush72@gmail.com>
2025-11-30 01:03:20 +0300

    feat(application): add GetUserByIdQueryValidator

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Get/GetUserByIdQueryValidator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Get/GetUserByIdQueryValidator.cs
@@ -1,6 +1,14 @@
+using FluentValidation;
+
 namespace InnoShop.UsersService.Application.Users.Get;
 
-public class GetUserByIdQueryValidator
+public class GetUserByIdQueryValidator :
+    AbstractValidator<GetUserByIdQuery>
 {
-    
+    public GetUserByIdQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .NotEmpty();
+    }
 }
