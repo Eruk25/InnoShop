e088f3794290e3db735af06affff8de344789f7e
Danila <danilamatush72@gmail.com>
2025-11-29 22:38:32 +0300

    feat(infrastructure): add PasswordResetToken to UserContext

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/DB/UserContext.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/DB/UserContext.cs
@@ -8,10 +8,13 @@ public class UserContext(DbContextOptions<UserContext> options) : DbContext(opti
 {
     public DbSet<User> Users { get; set; }
     public DbSet<EmailVerificationToken> EmailVerificationTokens { get; set; }
+    public DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
     public DbSet<OutBoxMessage> OutBoxMessages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new EmailVerificationTokenConfiguration());
+        modelBuilder.ApplyConfiguration(new PasswordResetTokenConfiguration());
     }
 }
