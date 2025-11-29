c64410ba909e48d532202442f8e9f4430d69c7ec
Danila <danilamatush72@gmail.com>
2025-11-29 22:52:12 +0300

    feat(presentation): register IPasswordResetLinkFactory in DI

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Extensions/DependencyInjection.cs
@@ -59,6 +59,7 @@ public static class DependencyInjection
             });
         services.AddAuthorization();
         services.AddScoped<IUrlGenerator, UrlGenerator>();
+        services.AddScoped<IPasswordResetLinkFactory, PasswordResetLinkFactory>();
         services.AddHttpContextAccessor();
         return services;
     }
