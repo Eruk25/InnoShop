7c17a22e39e8f256c97b8f9b278796154466c1f8
Danila <danilamatush72@gmail.com>
2025-11-30 01:03:38 +0300

    feat(application): add LoginUserCommandValidator

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Login/LoginUserCommandValidator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Login/LoginUserCommandValidator.cs
@@ -1,6 +1,17 @@
+using FluentValidation;
+
 namespace InnoShop.UsersService.Application.Users.Login;
 
-public class LoginUserCommandValidator
+public class LoginUserCommandValidator :
+    AbstractValidator<LoginUserCommand>
 {
-    
+    public LoginUserCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .NotEmpty();
+        RuleFor(x => x.Password)
+            .NotNull()
+            .NotEmpty();
+    }
 }
