2b0142ad8c52c4db96c828afbee707448f62d1a6
Danila <danilamatush72@gmail.com>
2025-11-29 22:51:04 +0300

    feat(applcation): add IPasswordResetLinkFactory interface

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/UrlGenerator/IPasswordResetLinkFactory.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/UrlGenerator/IPasswordResetLinkFactory.cs
@@ -1,6 +1,8 @@
+using InnoShop.UsersService.Domain.Entities;
+
 namespace InnoShop.UsersService.Application.Abstractions.UrlGenerator;
 
 public interface IPasswordResetLinkFactory
 {
-    
+    public string GenerateLink(PasswordResetToken token);
 }
