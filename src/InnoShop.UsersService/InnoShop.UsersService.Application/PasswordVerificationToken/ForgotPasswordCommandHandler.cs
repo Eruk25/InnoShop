e65d4db7d3a18b4f681b475a2952c0a1c53eba7b
Danila <danilamatush72@gmail.com>
2025-11-29 22:46:30 +0300

    feat(application): implement forgot password logic

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/PasswordVerificationToken/ForgotPasswordCommandHandler.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/PasswordVerificationToken/ForgotPasswordCommandHandler.cs
@@ -1,6 +1,44 @@
+using InnoShop.UsersService.Application.Abstractions.EmailSender;
+using InnoShop.UsersService.Application.Abstractions.Repositories;
+using InnoShop.UsersService.Application.Abstractions.UrlGenerator;
+using InnoShop.UsersService.Domain.Entities;
+using MediatR;
+
 namespace InnoShop.UsersService.Application.PasswordVerificationToken;
 
-public class ForgotPasswordCommandHandler
+public class ForgotPasswordCommandHandler : IRequestHandler<ForgotPasswordCommand>
 {
+    private readonly IPasswordResetTokenRepository _passwordResetTokenRepository;
+    private readonly IPasswordResetLinkFactory _passwordResetLinkFactory;
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailSender _emailSender;
     
+    public ForgotPasswordCommandHandler(IPasswordResetTokenRepository passwordResetTokenRepository, IUserRepository userRepository,
+        IEmailSender emailSender, IPasswordResetLinkFactory passwordResetLinkFactory)
+    {
+        _passwordResetTokenRepository = passwordResetTokenRepository;
+        _userRepository = userRepository;
+        _emailSender = emailSender;
+        _passwordResetLinkFactory = passwordResetLinkFactory;
+    }
+
+    public async Task Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+    {
+        if(request is null)
+            throw new ArgumentNullException(nameof(request));
+        
+        var user = await _userRepository.GetByEmailAsync(request.Email);
+        
+        if(user is null)
+            return;
+
+        var token = new PasswordResetToken(
+            Guid.NewGuid(),
+            user.Id,
+            DateTime.UtcNow.AddMinutes(20));
+        await _passwordResetTokenRepository.CreateAsync(token);
+
+        var verificationLink = _passwordResetLinkFactory.GenerateLink(token);
+        await _emailSender.SendEmailAsync(request.Email, $"To password reset <a href={verificationLink}>click here</a>");
+    }
 }
