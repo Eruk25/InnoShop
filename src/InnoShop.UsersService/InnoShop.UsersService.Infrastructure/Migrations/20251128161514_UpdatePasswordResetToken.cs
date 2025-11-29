2897ad06eb783cc05ca66daf90c9f433371389df
Danila <danilamatush72@gmail.com>
2025-11-29 22:32:36 +0300

    feat(infrastructure): add UpdatePasswordResetToken migration

---
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Migrations/20251128161514_UpdatePasswordResetToken.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoShop.UsersService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePasswordResetToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiresAt",
+                table: "PasswordResetTokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "PasswordResetTokens",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiresAt",
+                table: "PasswordResetTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "PasswordResetTokens");
+        }
+    }
+}
