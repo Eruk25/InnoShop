2397b94b451b8b3acd08b6cb98c7563d43c46603
Danila <danilamatush72@gmail.com>
2025-11-30 01:03:56 +0300

    feat(application): add RegisterUserCommandValidator

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Register/RegisterUserCommandValidator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Register/RegisterUserCommandValidator.cs
@@ -1,6 +1,22 @@
+using FluentValidation;
+
 namespace InnoShop.UsersService.Application.Users.Register;
 
-public class RegisteUserCommandValidator :
+public class RegisterUserCommandValidator : 
+    AbstractValidator<RegisterUserCommand>
 {
-    
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .MinimumLength(2)
+            .MaximumLength(20)
+            .NotEmpty();
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .NotEmpty();
+        RuleFor(x => x.Password)
+            .MinimumLength(6)
+            .MaximumLength(30)
+            .NotEmpty();
+    }
 }
