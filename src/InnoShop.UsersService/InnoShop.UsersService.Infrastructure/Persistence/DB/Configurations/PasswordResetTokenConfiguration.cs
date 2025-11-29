da1405dd36bf73753a55cc207675dd51f79eaed2
Danila <danilamatush72@gmail.com>
2025-11-29 22:39:23 +0300

    feat(infrastructure): add PasswordResetTokenConfiguration

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/DB/Configurations/PasswordResetTokenConfiguration.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/DB/Configurations/PasswordResetTokenConfiguration.cs
@@ -1,6 +1,18 @@
+using InnoShop.UsersService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace InnoShop.UsersService.Infrastructure.Persistence.DB.Configurations;
 
-public class PasswordResetTokenConfiguration
+public class PasswordResetTokenConfiguration : IEntityTypeConfiguration<PasswordResetToken>
 {
-    
+    public void Configure(EntityTypeBuilder<PasswordResetToken> builder)
+    {
+        builder.HasKey(t => t.Id);
+        
+        builder.Property(t => t.ExpiresAt)
+            .IsRequired();
+        builder.Property(t => t.UserId)
+            .IsRequired();
+    }
 }
