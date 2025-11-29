f82a9336a73ac940e218c322c344e12f7b707e8d
Danila <danilamatush72@gmail.com>
2025-11-30 02:07:14 +0300

    feat(presentation): add ExceptionHandlingMiddleware

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Middleware/ExceptionHandlingMiddleware.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,6 +1,67 @@
+using System.Text.Json;
+using InnoShop.UsersService.Application.Common.Exceptions;
+
 namespace InnoShop.UsersService.API.Middleware;
 
-public class ExceptionHandlingMiddleware
+public class ExceptionHandlingMiddleware : IMiddleware
 {
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
     
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger) => _logger = logger;
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var statusCode = GetStatusCode(exception);
+
+        var response = new
+        {
+            title = GetTitle(exception),
+            status = statusCode,
+            detail = exception.Message,
+            errors = GetErrors(exception)
+        };
+        
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+        
+        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+
+    private static int GetStatusCode(Exception exception) =>
+        exception switch
+        {
+            ValidationException => StatusCodes.Status400BadRequest,
+            BadRequestException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+    private static string GetTitle(Exception exception) =>
+        exception switch
+        {
+            ValidationException => "Validation Error",
+            BadRequestException => "Bad Request",
+            NotFoundException => "Not found",
+            _ => "Server error"
+        };
+
+    private static IReadOnlyDictionary<string, string[]>? GetErrors(Exception exception) =>
+        exception switch
+        {
+            ValidationException validation => validation.Errors,
+            _ => null
+        };
 }
