823e7b102ca1de999500db229eac2c36a53c6ade
Danila <danilamatush72@gmail.com>
2025-11-29 22:36:28 +0300

    feat(application): add IPasswordResetTokenRepository interface

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IPasswordResetTokenRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Abstractions/Repositories/IPasswordResetTokenRepository.cs
@@ -1,6 +1,10 @@
+using InnoShop.UsersService.Domain.Entities;
+
 namespace InnoShop.UsersService.Application.Abstractions.Repositories;
 
-public class IPasswordResetTokenRepository
+public interface IPasswordResetTokenRepository
 {
-    
+    public Task CreateAsync(PasswordResetToken passwordResetToken);
+    public Task<PasswordResetToken> GetByIdAsync(Guid id);
+    public Task DeleteAsync(PasswordResetToken passwordResetToken);
 }
