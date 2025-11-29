84287c99dc6a70dcdc0cda5e76ef1d80fb205420
Danila <danilamatush72@gmail.com>
2025-11-30 02:40:04 +0300

    feat(application): add UpdateProductCommandValidator

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Update/UpdateProductCommandValidator.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Products/Update/UpdateProductCommandValidator.cs
@@ -1,7 +1,23 @@
+using FluentValidation;
+
 namespace InnoShop.ProductsService.Application.Products.Update;
 
-public class UdpateProductCommandValidator :
-    
+public class UpdateProductCommandValidator :
+    AbstractValidator<UpdateProductCommand>
 {
-    
+    public  UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty();
+        RuleFor(x => x.Title)
+            .NotEmpty();
+        RuleFor(x => x.Description)
+            .MinimumLength(50)
+            .MaximumLength(200)
+            .NotEmpty();
+        RuleFor(x => x.Price)
+            .GreaterThan(0)
+            .LessThanOrEqualTo(100000)
+            .NotEmpty();
+    }
 }
