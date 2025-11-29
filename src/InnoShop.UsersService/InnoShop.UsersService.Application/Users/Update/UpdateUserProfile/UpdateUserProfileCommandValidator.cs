54cfcfb1a315d5e95f51e6851472997bc9f99be4
Danila <danilamatush72@gmail.com>
2025-11-30 01:05:26 +0300

    feat(application): add UpdateUserProfileCommandValidator

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserProfile/UpdateUserProfileCommandValidator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserProfile/UpdateUserProfileCommandValidator.cs
@@ -1,6 +1,21 @@
+using FluentValidation;
+
 namespace InnoShop.UsersService.Application.Users.Update.UpdateUserProfile;
 
-public class UpdateUserProfileCommandValidator
+public class UpdateUserProfileCommandValidator :
+    AbstractValidator<UpdateUserProfileCommand>
 {
-    
+    public UpdateUserProfileCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .NotEmpty();
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .NotEmpty();
+        RuleFor(x => x.Name)
+            .MinimumLength(2)
+            .MaximumLength(20)
+            .NotEmpty();
+    }
 }
