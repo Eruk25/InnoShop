3fee6cf4497ed0ae5b15258c1898d786e024fbbf
Danila <danilamatush72@gmail.com>
2025-11-29 22:40:48 +0300

    feat(application): add UpdateUserPasswordCommand

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserPassword/UpdateUserPasswordCommand.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserPassword/UpdateUserPasswordCommand.cs
@@ -1,3 +1,6 @@
+using MediatR;
+
 namespace InnoShop.UsersService.Application.Users.Update.UpdateUserPassword;
 
-public record UpdateUserPasswordCommand();
+public record UpdateUserPasswordCommand(string Email, string ResetCode, string NewPassword)
+    : IRequest;
