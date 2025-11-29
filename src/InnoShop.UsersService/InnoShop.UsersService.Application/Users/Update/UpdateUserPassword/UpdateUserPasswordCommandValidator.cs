4de3777fbc58e064a180690582dc6026e6b2c449
Danila <danilamatush72@gmail.com>
2025-11-30 01:05:10 +0300

    feat(application): add UpdateUserPasswordCommandValidator

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserPassword/UpdateUserPasswordCommandValidator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserPassword/UpdateUserPasswordCommandValidator.cs
@@ -1,6 +1,21 @@
+using FluentValidation;
+
 namespace InnoShop.UsersService.Application.Users.Update.UpdateUserPassword;
 
-public class UpdateUserPasswordCommandValidator
+public class UpdateUserPasswordCommandValidator :
+    AbstractValidator<UpdateUserPasswordCommand>
 {
-    
+    public UpdateUserPasswordCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .NotEmpty();
+        RuleFor(x => x.ResetCode)
+            .NotNull()
+            .NotEmpty();
+        RuleFor(x => x.NewPassword)
+            .MinimumLength(6)
+            .MaximumLength(30)
+            .NotEmpty();;
+    }
 }
