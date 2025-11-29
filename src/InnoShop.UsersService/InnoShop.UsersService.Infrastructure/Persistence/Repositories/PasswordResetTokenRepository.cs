2d9cf5e0381072cab9d9ea9bcb161f15a136ce18
Danila <danilamatush72@gmail.com>
2025-11-29 22:37:37 +0300

    feat(infrastructure): implement IPasswordResetTokenRepository in PasswordResetTokenRepository

--- src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/PasswordResetTokenRepository.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Infrastructure/Persistence/Repositories/PasswordResetTokenRepository.cs
@@ -1,6 +1,35 @@
+using InnoShop.UsersService.Application.Abstractions.Repositories;
+using InnoShop.UsersService.Domain.Entities;
+using InnoShop.UsersService.Infrastructure.Persistence.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace InnoShop.UsersService.Infrastructure.Persistence.Repositories;
 
-public class PasswordResetTokenRepository
+public class PasswordResetTokenRepository : IPasswordResetTokenRepository
 {
+    private readonly UserContext _context;
     
+    public PasswordResetTokenRepository(UserContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task CreateAsync(PasswordResetToken passwordResetToken)
+    {
+        await _context.PasswordResetTokens.AddAsync(passwordResetToken);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<PasswordResetToken> GetByIdAsync(Guid id)
+    {
+        var token = await _context.PasswordResetTokens.FirstAsync(t => t.Id == id);
+        return token;
+    }
+
+    public async Task DeleteAsync(PasswordResetToken passwordResetToken)
+    {
+        _context.PasswordResetTokens.Remove(passwordResetToken);
+        await _context.SaveChangesAsync();
+    }
 }
