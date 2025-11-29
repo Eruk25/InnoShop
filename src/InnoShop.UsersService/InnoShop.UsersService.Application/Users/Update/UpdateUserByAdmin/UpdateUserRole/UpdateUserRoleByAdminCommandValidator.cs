8d3368a2f8ac8473802ea0d1e2bf200daf40b868
Danila <danilamatush72@gmail.com>
2025-11-30 01:04:28 +0300

    feat(application): add UpdateUserRoleByAdminCommandValidator

--- src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserByAdmin/UpdateUserRole/UpdateUserRoleByAdminCommandValidator.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.Application/Users/Update/UpdateUserByAdmin/UpdateUserRole/UpdateUserRoleByAdminCommandValidator.cs
@@ -1,6 +1,17 @@
+using FluentValidation;
+
 namespace InnoShop.UsersService.Application.Users.Update.UpdateUserByAdmin.UpdateUserRole;
 
-public class UpdateUserRoleByAdminCommandValidator
+public class UpdateUserRoleByAdminCommandValidator :
+    AbstractValidator<UpdateUserRoleByAdminCommand>
 {
-    
+    public UpdateUserRoleByAdminCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .NotEmpty();
+        RuleFor(x => x.Role)
+            .NotNull()
+            .NotEmpty();
+    }
 }
