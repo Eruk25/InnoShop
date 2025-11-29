3079e6920b3ea42e3d21419ae883aae2fba4da17
Danila <danilamatush72@gmail.com>
2025-11-29 23:26:49 +0300

    feat(application): add ProductSearchCriteria argument to GetAll method

--- src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.Application/Abstractions/Repositories/IProductRepository.cs
@@ -1,10 +1,11 @@
+using InnoShop.ProductsService.Application.Products.Filters;
 using InnoShop.ProductsService.Domain.Entities;
 
 namespace InnoShop.ProductsService.Application.Abstractions.Repositories;
 
 public interface IProductRepository
 {
-    public Task<IEnumerable<Product>> GetAllAsync();
+    public Task<IEnumerable<Product>> GetAllAsync(ProductSearchCriteria filters);
     public Task<Product?> GetByIdAsync(int id);
     public Task<IEnumerable<Product>> GetByUserIdAsync(int userId);
     public Task CreateAsync(Product product);
