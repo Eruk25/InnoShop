eef8975437199a5f9f1a394859dde374d25e6bf4
Danila <danilamatush72@gmail.com>
2025-11-29 22:51:32 +0300

    feat(presentation): implement IPasswordResetLinkFactory in PasswordResetLinkFactory

--- src/InnoShop.UsersService/InnoShop.UsersService.API/Implementations/PasswordResetLinkFactory.cs
+++ src/InnoShop.UsersService/InnoShop.UsersService.API/Implementations/PasswordResetLinkFactory.cs
@@ -3,14 +3,25 @@ using InnoShop.UsersService.Domain.Entities;
 
 namespace InnoShop.UsersService.API.Implementations;
 
-public class PasswordResetUrlGenerator : IPasswordResetLinkFactory
+public class PasswordResetLinkFactory : IPasswordResetLinkFactory
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly LinkGenerator _linkGenerator;
-    
-    public PasswordResetUrlGenerator()
-    public string GenerateLink(Guid token)
+
+    public PasswordResetLinkFactory(IHttpContextAccessor httpContextAccessor, LinkGenerator linkGenerator)
     {
-        
+        _httpContextAccessor = httpContextAccessor;
+        _linkGenerator = linkGenerator;
+    }
+    
+    public string GenerateLink(PasswordResetToken token)
+    {
+        string? verificationLink = _linkGenerator.GetUriByAction(
+            action: "ResetPassword",
+            controller: "Auth",
+            values: new { token = token.Id},
+            scheme: _httpContextAccessor.HttpContext!.Request.Scheme,
+            host: _httpContextAccessor.HttpContext.Request.Host);
+        return verificationLink ?? throw new Exception("Could not generate link");
     }
 }
