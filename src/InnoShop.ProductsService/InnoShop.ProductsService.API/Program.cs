1608056a59f56673edb175c43d71d51d0796db5f
Danila <danilamatush72@gmail.com>
2025-12-01 05:04:22 +0300

    feat(presentation): add migrateAsync method to Di

--- src/InnoShop.ProductsService/InnoShop.ProductsService.API/Program.cs
+++ src/InnoShop.ProductsService/InnoShop.ProductsService.API/Program.cs
@@ -2,6 +2,8 @@ using InnoShop.ProductsService.API.Extensions;
 using InnoShop.ProductsService.API.Middleware;
 using InnoShop.ProductsService.Application.Extensions;
 using InnoShop.ProductsService.Infrastructure.Extensions;
+using InnoShop.ProductsService.Infrastructure.Persistence.DB;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<ProductContext>();
+await db.Database.MigrateAsync();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
